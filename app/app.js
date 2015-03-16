379654af4952539db44e1c322746a9ca7a286d91
akhardya <akhardya@gmail.com>
2015-03-16 12:45:31 +0100

    first setting of Coolocate app

--- app/app.js
+++ app/app.js
@@ -5,12 +5,12 @@ import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
+var Coolocate = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(Coolocate, config.modulePrefix);
 
-export default App;
+export default Coolocate;
